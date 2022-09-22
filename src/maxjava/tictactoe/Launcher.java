4221e402f172bc76daab6a69449f2e9b2e4e8d48
max-kubasov <kubasov87@gmail.com>
2022-09-22 15:04:23 +0300

    Separate the models and components

--- src/maxjava/tictactoe/Launcher.java
+++ src/maxjava/tictactoe/Launcher.java
@@ -22,7 +22,11 @@ public final class Launcher {
 
     public static void main(String[] args) {
         final Game game = new Game(
-                new DataPrinter(), new ComputerMove(), new UserMove(), new WinnerVerifier(), new DrawVerifier()
+                new DataPrinter(),
+                new ComputerMove(),
+                new UserMove(),
+                new WinnerVerifier(),
+                new DrawVerifier()
         );
 
         game.play();

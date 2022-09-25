b3c4ef6c01195bdb7617d633195d038277a9ddb8
Max Kubasov <kubasov87@gmail.com>
2022-09-25 12:24:59 +0300

    Implement random computer move

--- src/maxjava/tictactoe/component/ComputerMove.java
+++ src/maxjava/tictactoe/component/ComputerMove.java
@@ -16,10 +16,27 @@
 
 package maxjava.tictactoe.component;
 
+
+import maxjava.tictactoe.model.Cell;
 import maxjava.tictactoe.model.GameTable;
+
+import java.util.Random;
 
 public class ComputerMove {
 
     public void make(GameTable gameTable) {
+        final Random random = new Random();
+        while (true) {
+            final int row = random.nextInt(3);
+            final int col = random.nextInt(3);
+            final Cell randomCell = new Cell(row, col);
+
+            if (gameTable.isEmpty(randomCell)) {
+                gameTable.setSign(randomCell, '0');
+                return;
+            }
+        }
+
+
     }
 }

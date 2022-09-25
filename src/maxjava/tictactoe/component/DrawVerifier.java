cfc71ca303d4544ba2c7f08df3078905523290a9
Max Kubasov <kubasov87@gmail.com>
2022-09-25 22:46:53 +0300

    Implement DrawVerifier component

--- src/maxjava/tictactoe/component/DrawVerifier.java
+++ src/maxjava/tictactoe/component/DrawVerifier.java
@@ -16,11 +16,19 @@
 
 package maxjava.tictactoe.component;
 
+import maxjava.tictactoe.model.Cell;
 import maxjava.tictactoe.model.GameTable;
 
 public class DrawVerifier {
 
     public boolean isDraw(GameTable gameTable) {
-        return false;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (gameTable.isEmpty(new Cell(i, j))) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
